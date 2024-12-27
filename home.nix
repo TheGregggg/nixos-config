77f5c88a524b7bc8b8c0fee7e96967d5b5d84dbe
Grégoire Layet <git@gregoirelayet.com>
2024-12-27 18:14:05 +0100

    init config

---
+++ home.nix
@@ -0,0 +1,98 @@
+{ config, pkgs, ... }:
+
+{
+  # TODO please change the username & home directory to your own
+  home.username = "gregoire";
+  home.homeDirectory = "/home/gregoire";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    fastfetch
+
+    # archives
+    zip
+    unzip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    fzf # A command-line fuzzy finder
+
+    # networking tools
+    nmap # A utility for network discovery and security auditing
+
+    # misc
+    cowsay
+    which
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+
+    btop  # replacement of htop/nmon
+
+    # system tools
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "Grégoire Layet";
+    userEmail = "git@gregoirelayet.com";
+  };
+
+
+  programs.kitty.enable = true;
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # TODO add your custom bashrc here
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+      
+
+    # set some aliases, feel free to add more or remove some
+    shellAliases = {
+      ll = "ls -alh";
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
