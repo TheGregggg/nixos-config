d171b8441c1bccbcd061181ca67095b94bffa50b
Grégoire Layet <git@gregoirelayet.com>
2024-12-27 23:20:19 +0100

    add wallpaper

--- home/gnome.nix
+++ home/gnome.nix
@@ -1,4 +1,6 @@
 {...}: {
+  home.file."/home/gregoire/.local/share/backgrounds/wallpaper.png".source = ../wallpaper.png; # set wallpaper
+
   # gnome settings
   dconf = {
     enable = true;
@@ -7,6 +9,19 @@
       accent-color = "purple";
       enable-hot-corners = false;
       show-battery-percentage = true;
+    };
+
+    # set wallpaper - a lot of line because I just copy what the settings do to the dconf db
+    settings."/org/gnome/desktop/background" = {
+      picture-uri = "file:///home/gregoire/.local/share/backgrounds/wallpaper.png";
+      picture-uri-dark = "file:///home/gregoire/.local/share/backgrounds/wallpaper.png";
+      primary-color = "#000000000000";
+      secondary-color = "#000000000000";
+    };
+    settings."/org/gnome/desktop/screensaver" = {
+      picture-uri = "file:///home/gregoire/.local/share/backgrounds/wallpaper.png";
+      primary-color = "#000000000000";
+      secondary-color = "#000000000000";
     };
   };
 }
