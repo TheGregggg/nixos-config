1e5d36fd728fc5204c1979ddc490f88878762210
Grégoire Layet <git@gregoirelayet.com>
2024-12-28 16:55:44 +0100

    add ssh config

--- home/config/default.nix
+++ home/config/default.nix
@@ -7,4 +7,5 @@ in {
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink nvimPath;
   xdg.configFile."kitty".source = config.lib.file.mkOutOfStoreSymlink kittyPath;
   xdg.configFile."VSCodium/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink vscodiumPath;
+  home.file.".ssh/config".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/.ssh/config";
 }
