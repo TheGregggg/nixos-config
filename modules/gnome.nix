5e5a3f92d3ae77943d1da67c2aabdcc24f51f7dd
Grégoire Layet <git@gregoirelayet.com>
2025-01-15 00:58:24 +0100

    testing numpad activate at startup

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
@@ -32,5 +36,17 @@
   programs.kdeconnect = {
     enable = true;
     package = pkgs.gnomeExtensions.gsconnect;
+  };
+
+  systemd.services.numLockOnTty = {
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      # /run/current-system/sw/bin/setleds -D +num < "$tty";
+      ExecStart = lib.mkForce (pkgs.writeShellScript "numLockOnTty" ''
+        for tty in /dev/tty{1..6}; do
+            ${pkgs.kbd}/bin/setleds -D +num < "$tty";
+        done
+      '');
+    };
   };
 }
