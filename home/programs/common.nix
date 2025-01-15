bc93c432dcf2409e638590c735cc8396eaa3fb5e
Grégoire Layet <git@gregoirelayet.com>
2025-01-15 20:14:28 +0100

    add histcontrol ignore both in bashrc

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -81,6 +81,7 @@
     # TODO add your custom bashrc here
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      HISTCONTROL=ignoreboth
     '';
 
     # set some aliases, feel free to add more or remove some
