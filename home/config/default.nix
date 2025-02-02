d2783327559cb6d7b3afa33bba97fef65df34e62
Grégoire Layet <git@gregoirelayet.com>
2025-02-02 19:04:04 +0100

    add nerd font config to starship

--- home/config/default.nix
+++ home/config/default.nix
@@ -7,5 +7,6 @@ in {
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink nvimPath;
   # xdg.configFile."kitty".source = config.lib.file.mkOutOfStoreSymlink kittyPath;
   xdg.configFile."VSCodium/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink vscodiumPath;
+  xdg.configFile."starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/starship.toml";
   home.file.".ssh/config".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/.ssh/config";
 }
