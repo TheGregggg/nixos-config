7baa06cb629af094e41f12f51e0cb225cf028ede
Grégoire Layet <git@gregoirelayet.com>
2025-03-30 19:51:40 +0200

    remove kitty folder config cause its managed by nixos

--- home/config/default.nix
+++ home/config/default.nix
@@ -1,11 +1,9 @@
 {config, ...}: let
   nixConfigPath = "${config.home.homeDirectory}/nixos-config/home/config";
   nvimPath = "${nixConfigPath}/nvim";
-  # kittyPath = "${nixConfigPath}/kitty";
   vscodiumPath = "${nixConfigPath}/vscodium/settings.json";
 in {
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink nvimPath;
-  # xdg.configFile."kitty".source = config.lib.file.mkOutOfStoreSymlink kittyPath;
   xdg.configFile."VSCodium/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink vscodiumPath;
   xdg.configFile."starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/starship.toml";
   home.file.".ssh/config".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/.ssh/config";
