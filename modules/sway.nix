7f6117cb7087a31709442ea619b6c2948a8f68bf
Grégoire Layet <git@gregoirelayet.com>
2025-07-27 21:51:45 +0200

    continue configuring sway

--- modules/sway.nix
+++ modules/sway.nix
@@ -1,11 +1,10 @@
 {pkgs, ...}: {
   programs.dconf.enable = true;
-
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --remember --cmd ${pkgs.sway}/bin/sway";
         user = "gregoire";
       };
     };
@@ -28,7 +27,19 @@
     wrapperFeatures.gtk = true;
   };
 
+  programs.waybar.enable = true;
+
   # Enable the gnome-keyring secrets vault.
   # Will be exposed through DBus to programs willing to store secrets.
   services.gnome.gnome-keyring.enable = true;
+
+  services.udev.extraRules = ''
+    SUBSYSTEMS=="usb", SUBSYSTEM=="block", ACTION=="add", ENV{ID_FS_USAGE}=="filesystem", RUN+="${pkgs.writeShellScript "udev-mount.sh" ''
+      ${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --collect --options=X-mount.mkdir "$DEVNAME" "/run/media/$ID_FS_LABEL"
+    ''}"
+    SUBSYSTEMS=="usb", SUBSYSTEM=="block", ACTION=="remove", ENV{ID_FS_USAGE}=="filesystem", RUN+="${pkgs.writeShellScript "udev-unmount.sh" ''
+      ${pkgs.systemd}/bin/systemd-umount "$DEVNAME" "/run/media/$ID_FS_LABEL"
+      ${pkgs.coreutils}/bin/rmdir "/run/media/$ID_FS_LABEL"
+    ''}"
+  '';
 }
