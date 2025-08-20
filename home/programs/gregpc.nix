846e78c153f5493d09641a361f69e264cbaebf39
Grégoire Layet <git@gregoirelayet.com>
2025-08-20 19:22:03 +0200

    update

--- home/programs/gregpc.nix
+++ home/programs/gregpc.nix
@@ -5,7 +5,6 @@
 
   home.packages = with pkgs; [
     gnome-screenshot
-
-    steam
+    nicotine-plus
   ];
 }
--- hosts/gregpc/default.nix
+++ hosts/gregpc/default.nix
@@ -5,6 +5,7 @@
 
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./system-apps.nix
   ];
 
   # Bootloader config
