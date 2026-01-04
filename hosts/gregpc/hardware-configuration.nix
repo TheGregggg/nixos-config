991a4de50dea8d211f436630909c77b734fdb320
Grégoire Layet <git@gregoirelayet.com>
2026-01-04 01:16:01 +0100

    added kernel modules for sensors PC

--- hosts/gregpc/hardware-configuration.nix
+++ hosts/gregpc/hardware-configuration.nix
@@ -14,7 +14,7 @@
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = [];
+  boot.kernelModules = ["k10temp" "it87"];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
