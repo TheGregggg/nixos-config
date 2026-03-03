ef11c97a76ac4c5d28cf9494eeb5c0ed1f5c4fbd
Grégoire Layet <git@gregoirelayet.com>
2026-03-03 17:28:09 +0100

    add udev rule for switch game dumping

--- hosts/gregpc/default.nix
+++ hosts/gregpc/default.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   imports = [
     ../../modules/system.nix
     ../../modules/gnome.nix
@@ -59,6 +63,20 @@
   networking.networkmanager.enable = true;
 
   #ncfg.services.numlock-on-tty.enable = true;
+
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "71-nxdumptool.rules";
+      destination = "/etc/udev/rules.d/71-nxdumptool.rules";
+      text = ''
+            # Nintendo SDK debugger, which nxdumptool presents itself as
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="3000", TAG+="uaccess"
+      '';
+      checkPhase = ''
+        ${config.systemd.package}/bin/udevadm verify --resolve-names=late $out/etc/udev/rules.d/71-nxdumptool.rules
+      '';
+    })
+  ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
