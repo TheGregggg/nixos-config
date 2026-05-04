fee8dffcc1d681179f2604d8e79fbeae4e84bae8
Grégoire Layet <git@gregoirelayet.com>
2026-05-04 15:59:53 +0200

    added disk encryption

--- hosts/gregwork/default.nix
+++ hosts/gregwork/default.nix
@@ -1,7 +1,11 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{pkgs, lib, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     ../../modules/system.nix
     ../../modules/gnome.nix
@@ -13,40 +17,42 @@
   ];
 
   # Bootloader config
-  boot.loader = {
-    systemd-boot.enable = false;
+  boot = {
+    loader = {
+      systemd-boot.enable = false;
 
-    grub = {
-      enable = true;
-      device = "nodev";
-      useOSProber = true;
-      efiSupport = true;
-      configurationLimit = 10; # Limit the number of generations to keep
-      theme = pkgs.stdenv.mkDerivation {
-        pname = "CyberGRUB-2077";
-        version = "1";
-        src = pkgs.fetchFromGitHub {
-          owner = "TheGregggg";
-          repo = "CyberGRUB-2077";
-          rev = "f95fa64b062e8d965385cc0ca89abd1970b19742";
-          hash = "sha256-IqSzQ4UPpxX7oMfuOdgcSj6rUNjsrXJVGnLhvbbOMVk=";
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        configurationLimit = 10; # Limit the number of generations to keep
+        theme = pkgs.stdenv.mkDerivation {
+          pname = "CyberGRUB-2077";
+          version = "1";
+          src = pkgs.fetchFromGitHub {
+            owner = "TheGregggg";
+            repo = "CyberGRUB-2077";
+            rev = "f95fa64b062e8d965385cc0ca89abd1970b19742";
+            hash = "sha256-IqSzQ4UPpxX7oMfuOdgcSj6rUNjsrXJVGnLhvbbOMVk=";
+          };
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/
+
+            cp -r CyberGRUB-2077/* $out/
+
+            runHook postInstall
+          '';
         };
-        installPhase = ''
-          runHook preInstall
+      };
 
-          mkdir -p $out/
-
-          cp -r CyberGRUB-2077/* $out/
-
-          runHook postInstall
-        '';
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
       };
     };
-
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
-    };
+    initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/11425bca-ef57-4b7e-8991-9d563561f4d4";
   };
 
   networking.hostName = "gregwork"; # Define your hostname.
@@ -55,9 +61,9 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  
+
   services.xserver.xkb = {
-    layout = lib.mkForce  "us";
+    layout = lib.mkForce "us";
     variant = "";
   };
 
