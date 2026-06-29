0899417393e3f92909c233080a87a3902084b5b9
Grégoire Layet <git@gregoirelayet.com>
2026-06-29 19:02:40 +0200

    remove unecesarry nixos hardware
    Signed-off-by: Grégoire Layet <git@gregoirelayet.com>

--- make-sd-image.sh
+++ make-sd-image.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 nixpkgs_pin=$(nix --extra-experimental-features nix-command eval --raw -f npins/default.nix nixpkgs)
-nixos_hardware_pin=$(nix --extra-experimental-features nix-command eval --raw -f npins/default.nix nixos-hardware)
-nix_path="nixpkgs=${nixpkgs_pin}:nixos-hardware=${nixos_hardware_pin}"
+nix_path="nixpkgs=${nixpkgs_pin}"
 
 env NIX_PATH="${nix_path}" env HOSTNAME="rpi" nix-build '<nixpkgs/nixos>' -A config.system.build.sdImage -I nixos-config=./sd-image.nix --argstr system aarch64-linux
